d355be4a5d9abdb93f0f105c7ecedf5a7d33c69f
Nicolai Bonde <neobonde@gmail.com>
2019-08-10 23:53:01 +0200

    Chain hand shooting implemented

--- Player/ChainArm/Scripts/Chain.gd
+++ Player/ChainArm/Scripts/Chain.gd
@@ -3,11 +3,12 @@ extends Node2D
 const LOOP = preload("res://Player/ChainArm/Loop.tscn")
 const LINK = preload("res://Player/ChainArm/Link.tscn")
 const HAND = preload("res://Player/ChainArm/Hand.tscn")
+const EXTRA_CHAINS = 4
 
 export (int) var loops = 1
 
-var hand
-var last_child
+var chainList = []
+var chainHand
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -16,29 +17,69 @@ func _ready():
 		var child = addLoop(parent)
 		addLink(parent, child)
 		parent = child
-		last_child = child
-	hand = addHand(parent)
+	var hand = addHand(parent)
+	addLink(parent, hand, true)
 
 func addLoop(parent):
 	var loop = LOOP.instance()
 	loop.position = parent.position
-	loop.position.x += 13
+	loop.position.x += 26
 	add_child(loop)
+	chainList.append(loop)
 	return loop
-	
-func addHand(parent):
-	var loop = HAND.instance()
-	loop.position = parent.position
-	add_child(loop)
-	return loop
-	
-func addLink(parent, child):
+
+func addLink(parent, child, hide=false):
 	var pin = LINK.instance()
+	pin.get_node("Sprite").visible = !hide
 	pin.node_a = parent.get_path()
 	pin.node_b = child.get_path()
 	parent.add_child(pin)
+	chainList.append(pin)
+	
+func addHand(parent):
+	var hand = HAND.instance()
+	hand.position = parent.position
+	add_child(hand)
+	hand.connect("retracted",self,"on_retracted")
+	chainHand = hand
+	return hand
 
-func _physics_process(delta):
-	if hand != null:
-		pass
-	pass
+# This remove both a loop and a link
+func removeLoopItem():
+	var item = chainList.pop_back()
+	remove_child(item)
+	item.queue_free()
+
+func removeHand():
+	if chainHand != null:
+		chainHand.disconnect("retracted",self,"on_retracted")
+		remove_child(chainHand)
+		chainHand.queue_free()
+		chainHand = null
+		removeLoopItem()
+
+func on_retracted():
+	extended = false
+	removeHand()
+	for i in range(EXTRA_CHAINS):
+		removeLoopItem()
+		removeLoopItem()
+	var parent = chainList[chainList.size()-2]
+	var hand = addHand(parent)
+	addLink(parent, hand, true)
+
+var extended = false
+
+func _process(delta):
+	if Input.is_action_just_pressed("player_fire") and not extended:
+		extended = true
+		removeHand()
+		var child = chainList[chainList.size()-1]
+		var parent = chainList[chainList.size()-2]
+		for i in range(EXTRA_CHAINS):
+			child = addLoop(parent)
+			addLink(parent, child)
+			parent = child
+		var hand = addHand(parent)
+		addLink(parent, hand, true)
+		hand.fire(26*2*(EXTRA_CHAINS))
--- Player/ChainArm/Scripts/Hand.gd
+++ Player/ChainArm/Scripts/Hand.gd
@@ -1,6 +1,9 @@
 extends RigidBody2D
 
 const DISTANCE = 46
+const MIN_DISTANCE = DISTANCE -30
+const FORCE = 1500
+const RETRACTION_SPEED = 3
 
 var pivot 
 var spawned_hand
@@ -19,18 +22,46 @@ func hand_connect(hand, collider):
 	collider.add_child(_h)
 	spawned_hand = _h
 
+var detached = false
+var fired = false
+var extended_length = 0
+var retracting = false
+var retraction = 0
+var retraction_start = Vector2()
+signal retracted  
+signal extended
+
+func fire(ext_len):
+	extended_length = ext_len
+	detached = true
+	set_deferred("mode",MODE_RIGID)
+
+
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _physics_process(delta):
 	aim_vector = (get_global_mouse_position() - pivot.global_position).normalized()
-	position = aim_vector * DISTANCE + pivot.position
-	$Sprite.look_at(get_global_mouse_position())
-	
-#	if Input.is_action_just_pressed("player_fire"):
-#		if spawned_hand == null:
-#			spawned_hand = hand.instance()
-#			spawned_hand.start(position,0, aim_vector)
-#			spawned_hand.arm = self
-#			get_parent().add_child(spawned_hand)
-#		else:
-#			spawned_hand.queue_free()
-#			spawned_hand = null
+	if not detached:
+		if pivot.global_position.distance_to(get_global_mouse_position()) > MIN_DISTANCE:
+			position = aim_vector * DISTANCE + pivot.position
+			$Sprite.look_at(get_global_mouse_position())
+	elif not fired and detached:
+			apply_impulse(Vector2(),aim_vector*FORCE*mass)
+			fired = true
+	elif detached:
+		if pivot.global_position.distance_to(global_position) >= extended_length:
+			# retract
+			retracting = true
+			set_deferred("mode",MODE_KINEMATIC)
+			emit_signal("extended")
+			retraction_start = global_position
+		if retracting:
+			var goal = aim_vector * DISTANCE + pivot.global_position
+			var temp = retraction_start.linear_interpolate(goal, retraction)
+			global_position = temp
+			retraction += delta * RETRACTION_SPEED
+			if retraction >= 0.9:
+				retraction = 0
+				retracting = false
+				detached = false
+				fired = false
+				emit_signal("retracted")
